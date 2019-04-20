9059ca5ddff4c7344432abc6ee4fca481c133453
dave <dave@bould.net>
2019-04-20 12:08:12 +0100

    Use string util library to reverse a string

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dbould/string-util"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
